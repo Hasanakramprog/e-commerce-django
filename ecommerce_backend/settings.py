fbb1fed421ee629f83c14c535d449720db54f496
hassan <hassanakranprog@gmail.com>
2024-05-27 05:53:26 +0300

    ready for deploy

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,13 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/thumbnail/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'thumbnail')
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'thumbnail')
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
-
+# STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
